1c1e21970326ba63e4d5feeb4a8b38d91c9e0491
daniyal <daniyal.majeed1606@ucp.edu.pk>
2017-08-16 19:17:57 +0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+app.set('view engine','hbs')
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text)=>{
+  return text.toUpperCase();
+});
+
+app.use((req,res,next)=>{
+  var now  = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + '\n',(err)=>{
+    if(err){
+      console.log('Unable to write to the server.log');
+    }
+  });
+
+  next();
+
+});
+
+// app.use((req,res,next)=>{
+//   res.render('maintainance.hbs')
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res)=>{
+  // res.send({
+  //   name: 'Daniyal',
+  //   likes: [
+  //     'Eat', 'Sleep','Repeat'
+  //   ]
+  // });
+  res.render('home.hbs',{
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome To My Website'
+  })
+});
+
+app.get('/bad',(req,res)=>{
+
+res.send({
+  eroor: 'Web page not availbale'
+});
+});
+
+app.get('/about',(req,res)=>{
+
+//res.send('<h1>My first web Page in node js </h1>');
+res.render('about.hbs',{
+  pageTitle: 'About Page'
+})
+});
+
+app.listen(3000,()=>{
+  console.log('Server is set Up');
+});
